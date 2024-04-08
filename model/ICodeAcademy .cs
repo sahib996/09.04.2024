0d9cc251ba029125507189c0db366e347c0d8794
sahib memmedov <sahibmemmedov7890@gmail.com>
2024-04-08 12:11:41 -0700

    initial commit

---
+++ model/ICodeAcademy .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._04._2024.model
+{
+    internal interface ICodeAcademy
+    {
+        string CodeEmail { get; set; }
+        void GenerateEmail();
+    }
+}
---
+++ model/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._04._2024.model
+{
+    internal class Student
+    {
+        private static int Count = 0;
+        private int Id;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+            Id = Count++;
+             
+            public static bool CheckName(string name)
+            {
+                return
+            }
+        }
+    }
+}
